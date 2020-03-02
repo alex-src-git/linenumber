8926dee13781a95db38822509af2ffaf233a9763
unknown <alexander.daley.me@gmail.com>
2020-03-02 15:56:39 +0000

    Wrote deduce_indentation function and tests

--- linenumber.py
+++ linenumber.py
@@ -1,2 +1,8 @@
 def is_none_or_whitespace(string):
     return not string or string.isspace()
+
+def deduce_indentation(string):
+    for (i, char) in enumerate(string):
+        if (not char.isspace()):
+            return i
+    return 0
--- linenumber_tests.py
+++ linenumber_tests.py
@@ -14,5 +14,19 @@ class IsNoneOrWhitespaceTests(unittest.TestCase):
     def test_returns_false_when_arg_is_non_empty_string(self):
         self.assertFalse(linenumber.is_none_or_whitespace("Hello World"))
 
+class DeduceIndentationTests(unittest.TestCase):
+    def test_returns_0_when_arg_is_empty_string(self):
+        self.assertEqual(0, linenumber.deduce_indentation(""))
+    
+    def test_returns_0_when_arg_is_whitespace(self):
+        self.assertEqual(0, linenumber.deduce_indentation("    "))
+
+    def test_returns_indentation_when_arg_is_indented_string(self):
+        self.assertEqual(4, linenumber.deduce_indentation("    :"))
+
+    def test_raises_type_error_when_arg_is_none(self):
+        with self.assertRaises(TypeError):
+            linenumber.deduce_indentation(None)
+
 if __name__ == '__main__':
     unittest.main()
